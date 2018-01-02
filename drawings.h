9ae9136d36d572832fcd7a1ed24060c9b1d6e3c6
Matt <supermasecla22@yahoo.com>
2018-01-02 19:01:19 +0200

    Added some positioning mechanics (also added food)

--- drawings.h
+++ drawings.h
@@ -1,16 +1,37 @@
 #include <graphics.h>
 #include <iostream>
 #include <cmath>
+#define UP 1
+#define LEFT 2
+#define RIGHT 3
+#define DOWN 4
+
+
 
 using namespace std;
 
-int snkRect(int x,int y)
+
+cord getPos(cord pos,int direct)
+{
+    if(direct==UP)
+        pos.y--;
+    if(direct==DOWN)
+        pos.y++;
+    if(direct==LEFT)
+        pos.x--;
+    if(direct==RIGHT)
+        pos.x++;
+    return pos;
+}
+
+
+void snkRect(int x,int y)
 {
     x--;
     y--;
     rectangle(x*20,y*20,x*20+20,y*20+20);
 }
-int snkColor(int x,int y)
+void snkColor(int x,int y)
 {
     x--;
     y--;
@@ -25,26 +46,48 @@ struct cord
 {
     int x,y;
 };
+struct food
+{
+    cord pos;
+    void drawIt()
+    {
+        ellipse(pos.x*20+10,pos.y*20+10,360,360,10,10);
+    }
+
+};
+void moveUp(cord poss[100],int lenght)
+{
+    cord newArray[100];
+    for(int i=0;i<lenght;i++)
+    {
+        newArray[i+1]=poss[i];
+    }
+    newArray[0]=getPos(poss[0],UP);
+}
+
+
+
+
+
+
+
 
 
 struct snake
 {
     int lenght;
     cord poss[100];
-    int drawIt()
+    void drawIt()
     {
         for(int i=0; i<lenght; i++)
-        {
             snkColor(poss[i].x,poss[i].y);
+    }
+    void nextMove(int direction)
+    {
+        if(direction==1)
+        {
+            moveUp(poss,lenght);
         }
     }
-
-
-
 };
 
-struct food {
-
-
-
-};
--- main.cpp
+++ main.cpp
@@ -2,11 +2,19 @@
 #include <graphics.h>
 #include "drawings.h"
 using namespace std;
-
+void showGrid()
+{
+    for(int i=0;i<46;i++)
+    for(int j=0;j<31;j++)
+        snkRect(i,j);
+}
 int main()
 {
     initwindow(900,600,"Snake -by masecla22",100,100);
+    while(true)
+    {
 
+    }
     getch();
     return 0;
 }

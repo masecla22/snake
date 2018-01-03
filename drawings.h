470a6889f8ffad9c790203d5b06154b3049c0d25
Matt <supermasecla22@yahoo.com>
2018-01-03 17:58:25 +0200

    Added food and moveing mechanics

--- drawings.h
+++ drawings.h
@@ -9,8 +9,26 @@
 
 
 using namespace std;
+struct cord
+{
+    int x,y;
+};
+struct food
+{
+    cord pos;
+    void init()
+    {
+        pos.x=2;
+        pos.y=3;
+    }
+    void drawIt()
+    {
+        ellipse(pos.x*20+10,pos.y*20+10,360,360,10,10);
+    }
 
+};
 
+///getPos - done
 cord getPos(cord pos,int direct)
 {
     if(direct==UP)
@@ -24,13 +42,15 @@ cord getPos(cord pos,int direct)
     return pos;
 }
 
-
+/// snkRect - done
 void snkRect(int x,int y)
 {
     x--;
     y--;
     rectangle(x*20,y*20,x*20+20,y*20+20);
 }
+
+///snkColor - done
 void snkColor(int x,int y)
 {
     x--;
@@ -41,36 +61,15 @@ void snkColor(int x,int y)
     }
 }
 
-
-struct cord
-{
-    int x,y;
-};
-struct food
-{
-    cord pos;
-    void drawIt()
-    {
-        ellipse(pos.x*20+10,pos.y*20+10,360,360,10,10);
-    }
-
-};
-void moveUp(cord poss[100],int lenght)
+void moves(cord poss[100],int lenght, int direct)
 {
     cord newArray[100];
     for(int i=0;i<lenght;i++)
-    {
         newArray[i+1]=poss[i];
-    }
-    newArray[0]=getPos(poss[0],UP);
+    newArray[0]=getPos(poss[0],direct);
+    for(int i=0;i<lenght;i++)
+        poss[i]=newArray[i];
 }
-
-
-
-
-
-
-
 
 
 struct snake
@@ -84,9 +83,24 @@ struct snake
     }
     void nextMove(int direction)
     {
-        if(direction==1)
+        moves(poss,lenght,direction);
+    }
+    void init()
+    {
+        for(int i=0;i<lenght;i++)
         {
-            moveUp(poss,lenght);
+            poss[i].x=(i+1);
+            poss[i].y=2;
+        }
+    }
+    void detectColision(food eat[100])
+    {
+        for(int i=0;i<lenght;i++)
+        {
+            if(eat[i].pos.x==poss[i].x&&eat[i].pos.y==poss[i].y)
+            {
+                lenght++;
+            }
         }
     }
 };
--- main.cpp
+++ main.cpp
@@ -1,19 +1,48 @@
 #include <iostream>
 #include <graphics.h>
+#include <winuser.h>
 #include "drawings.h"
 using namespace std;
 void showGrid()
 {
-    for(int i=0;i<46;i++)
-    for(int j=0;j<31;j++)
-        snkRect(i,j);
+    for(int i=0; i<46; i++)
+        for(int j=0; j<31; j++)
+            snkRect(i,j);
 }
 int main()
 {
     initwindow(900,600,"Snake -by masecla22",100,100);
+    int drect=UP;
+    snake player;
+    player.lenght=9;
+    player.init();
+    food eats[10];
+    for(int i=0; i<10; i++)
+    {
+        eats[i].init();
+        eats[i].drawIt();
+    }
+    int a=2;
     while(true)
     {
-
+        if(GetAsyncKeyState(VK_UP)!=0)
+            drect=UP;
+        if(GetAsyncKeyState(VK_DOWN)!=0)
+            drect=DOWN;
+        if(GetAsyncKeyState(VK_LEFT)!=0)
+            drect=LEFT;
+        if(GetAsyncKeyState(VK_RIGHT)!=0)
+            drect=RIGHT;
+        player.drawIt();
+        Sleep(100);
+        player.nextMove(drect);
+        cleardevice();
+        player.drawIt();
+        player.detectColision(eats);
+        for(int i=0; i<10; i++)
+            eats[i].drawIt();
+        cout<<drect<<endl;
+        a++;
     }
     getch();
     return 0;
